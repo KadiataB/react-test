eacc82428f3adfb4263cd254db8dee50b4e621f5
KadiataB <kadiatab79@gmail.com>
2024-11-01 18:55:59 +0000

    react-test

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div  class=" w-full h-full  text-center">
+      {/* <h1 class="h-96">Bonjour, Moussa!</h1> */}
+      <nav class="  *:  *:           p-5 bg-blue-400 ">
+        <div class="flex justify-between items-end">
+        <p>kadia</p>
+        <p>moussa</p>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
